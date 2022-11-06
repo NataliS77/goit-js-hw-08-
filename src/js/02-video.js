56169e7a1958c1a8a4c417d3a9e095c23234b5a4
NataliS77 <107251503+NataliS77@users.noreply.github.com>
2022-11-06 17:48:55 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,19 @@
 import Player from '@vimeo/player';
-console.log(Player)
+// console.log(Player)
 
 const iframe = document.querySelector('iframe')
 const player = new Player(iframe)
 const key = "videoplayer-current-time"
 
 player.on('play', function() {
-   console.log('played the video!');
+//    console.log('played the video!');
  });
     
 player.getVideoTitle().then(function(title) {
-     console.log('title:', title);
+     // console.log('title:', title);
    
 });
  player.on('timeupdate', function(data) {
        localStorage.setItem("videoplayer-current-time",JSON.stringify(data.seconds))
     });   
+    console.log(localStorage)
